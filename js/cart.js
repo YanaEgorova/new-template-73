43e9ce57d3b3593e00f93e880925e13ef0bc5344
Yana <yanaegorova69.3@gmail.com>
2022-01-18 02:01:50 +0200

    +

---
+++ js/cart.js
@@ -0,0 +1,9 @@
+import { products } from './data/products.js';
+import { item} from './templates/product-item.js';
+
+const productsList = document.querySelector('.js_products__list');
+const heroList = document.querySelector('.js_products__list-hero');
+
+products.forEach(product => {
+    productsList.insertAdjacentHTML('beforeend',item(product));
+})
